d830c7b995cafed0b55cf21505b9f0eea4bb9ab5
James Grant <grantjl@cardiff.ac.uk>
2019-03-16 11:11:58 +0000

    Initial creation of files. Created an argument parser

---
+++ netHack.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3
+import argparse
+import multiprocessing
+
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ip', "-i",default="127.0.0.1", type=str, help="Target IP address")
+    # parser.add_argument('--ip', "-i",default="127.0.0.1", type=str, help="Target IP address")
+    return parser.parse_args()
+
+def main():
+    args = parseArgs()
+    return 0
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
